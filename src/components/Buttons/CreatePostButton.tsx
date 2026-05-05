c3587339ef38100575417cc1bc581c2357fb699d
daniguti99 <dgm1021999@gmail.com>
2026-05-05 14:10:56 +0200

    Subida de fix de arreglo de importación css

--- src/components/Buttons/CreatePostButton.tsx
+++ src/components/Buttons/CreatePostButton.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import "../../css/createPost/createPostButton.css";
-
+import "../../css/createPost/CreatePostButton.css";
 export default function CreatePostButton() {
   const navigate = useNavigate();
 
